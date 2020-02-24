554370e0f3e42911f67aaa4efa1800cbdd3dfabe
Redhmam Pablo <redhmam@hotmail.com>
2020-02-24 00:40:07 +0000

    Logged user fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,7 @@ class UserController extends Controller
 
     public function getLoggedUser(Request $request){
         return response()->json([
-            'user' => Auth::user()->first()->toArray()
+            'user' => Auth::user()->toArray()
         ]);
     }
 }
